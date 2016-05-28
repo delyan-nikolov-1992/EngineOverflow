ca01982fd7ddf9a9d20103808ae7bcfa1342119c
Delyan Nikolaev Nikolov <delyan.nikolov.1992@gmail.com>
2016-05-28 15:25:16 +0300

    Implemented automatic CreatedOn, DeletedOn, ModifiedOn data fields Added model base classes and interfaces (IAuditInfo, IDeletableEntity) Added model Tag

---
+++ Source/Data/EngineOverflow.Data.Common/Models/IOrderable.cs
@@ -0,0 +1,7 @@
+﻿namespace EngineOverflow.Data.Common.Models
+{
+    public interface IOrderable
+    {
+        int OrderBy { get; set; }
+    }
+}
--- Source/Data/EngineOverflow.Data/ApplicationDbContext.cs
+++ Source/Data/EngineOverflow.Data/ApplicationDbContext.cs
@@ -1,5 +1,10 @@
 ﻿using EngineOverflow.Data.Models;
+using EngineOverflow.Data.Migrations;
+using EngineOverflow.Data.Common.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Linq;
+using System;
 namespace EngineOverflow.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -7,11 +12,62 @@ namespace EngineOverflow.Data
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public IDbSet<Tag> Tags { get; set; }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            this.ApplyDeletableEntityRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditInfo)entry.Entity;
+
+                if (entry.State == EntityState.Added)
+                {
+                    if (!entity.PreserveCreatedOn)
+                    {
+                        entity.CreatedOn = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
+        }
+
+        private void ApplyDeletableEntityRules()
+        {
+            // Approach via @julielerman: http://bit.ly/123661P
+            foreach (
+                var entry in
+                    this.ChangeTracker.Entries()
+                        .Where(e => e.Entity is IDeletableEntity && (e.State == EntityState.Deleted)))
+            {
+                var entity = (IDeletableEntity)entry.Entity;
+
+                entity.DeletedOn = DateTime.Now;
+                entity.IsDeleted = true;
+                entry.State = EntityState.Modified;
+            }
         }
     }
 }
