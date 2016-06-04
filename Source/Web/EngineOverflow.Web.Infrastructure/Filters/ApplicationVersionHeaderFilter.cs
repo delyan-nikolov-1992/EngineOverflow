72404761cf175fe7f1ce0e464f60117cb92d1e07
Delyan Nikolaev Nikolov <delyan.nikolov.1992@gmail.com>
2016-06-04 18:25:11 +0300

    added application filter and some performance improvements

---
+++ Source/Web/EngineOverflow.Web.Infrastructure/Filters/ApplicationVersionHeaderFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace EngineOverflow.Web.Infrastructure.Filters
+{
+    using System.Web.Mvc;
+
+    public class ApplicationVersionHeaderFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            filterContext.HttpContext.Response.Headers.Add("Application", "EngineOverflow");
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
--- Source/Web/EngineOverflow.Web/App_Start/FilterConfig.cs
+++ Source/Web/EngineOverflow.Web/App_Start/FilterConfig.cs
@@ -2,11 +2,14 @@
 {
     using System.Web.Mvc;
 
+    using EngineOverflow.Web.Infrastructure.Filters;
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new ApplicationVersionHeaderFilter());
         }
     }
 }
